2707f5b8a3b5b5c2f722abdd293f0b469dde79c6
m-byteforge <mekor.uia.no>
2024-01-16 21:31:28 +0100

    initial commit with colorful joke

---
+++ app.js
@@ -0,0 +1,9 @@
+const joke = chalk.red('Why') + ' ' +
+             chalk.yellow('did') + ' ' +
+             chalk.green('the') + ' ' +
+             chalk.blue('chicken') + ' ' +
+             chalk.magenta('join') + ' ' +
+             chalk.cyan('a') + ' ' +
+             chalk.white('band') + '?';
+
+console.log(joke);
