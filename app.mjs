b7606ecd551343feab1df5b8e705613c3a0ebd6a
m-byteforge <mekor.uia.no>
2024-01-16 21:53:50 +0100

    Update file extension to .mjs

---
+++ app.mjs
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const joke = chalk.red('Why') + ' ' +
+             chalk.yellow('did') + ' ' +
+             chalk.green('the') + ' ' +
+             chalk.blue('chicken') + ' ' +
+             chalk.magenta('join') + ' ' +
+             chalk.cyan('a') + ' ' +
+             chalk.white('band') + '?';
+
+console.log(joke);
